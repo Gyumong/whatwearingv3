9f987682dfd1b36ffb1e73d1219859844137aa81
Gyumong <rlaalsrb3559@naver.com>
2020-11-09 17:05:09 +0900

    Updates

---
+++ precache-manifest.e9e276553adc635b4e55e96136be0e19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6850cc46d8aee08a93e53eeca3425379",
+    "url": "/whatwearingv3/index.html"
+  },
+  {
+    "revision": "59efa17d7ee5b6ecb750",
+    "url": "/whatwearingv3/static/css/2.6fe1918f.chunk.css"
+  },
+  {
+    "revision": "bbf937afdab6772eb6e6",
+    "url": "/whatwearingv3/static/css/main.ff4b66b8.chunk.css"
+  },
+  {
+    "revision": "59efa17d7ee5b6ecb750",
+    "url": "/whatwearingv3/static/js/2.e3aaf733.chunk.js"
+  },
+  {
+    "revision": "572bfeb62474f0cec44e259ca5a99f18",
+    "url": "/whatwearingv3/static/js/2.e3aaf733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf937afdab6772eb6e6",
+    "url": "/whatwearingv3/static/js/main.cc343915.chunk.js"
+  },
+  {
+    "revision": "17cae13ecfbd96125fa4",
+    "url": "/whatwearingv3/static/js/runtime-main.4d0b056f.js"
+  }
+]);
